ca662adfaaad50787ded51d2c69b24c0a2781717
Tato <ayadalshaikhli@gmail.com>
2021-05-17 01:21:15 -0700

    made the website responsive added more animation to the sections changed the social media to be fixed on the website added more experience

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio-3
+
+https://ayadalshaikhli.github.io/Portfolio-3/
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,16 +37,126 @@ tm.to(".box", 2.4, {
     ease: Expo.easeInOut,
 });
 
+tm.from(".navbar > div", 1.6, {
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+});
+tm.from(".social", 1.6, {
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut,
+    delay: 0.6,
+});
 
-gsap.to(".about", {
+// =======================
+// Secound Section Animation
+// =======================
+gsap.from(".about", {
     scrollTrigger:{
     trigger: ".about",
-    markers: true,
-    toggleActions: "restart pause resume pause"
+    toggleActions: "restart resume restart resume"
     } ,
-    scale: "1.1",
-    ease: Expo.easeInOut,
+    opacity: 0,
+    delay: 4,
+    duration: 4
+
+})
+
+gsap.from(".block-5", {
+    scrollTrigger:{
+    trigger: ".block-5",
+    toggleActions: "restart resume resume restart"
+    } ,
+    x: 600,
+    y: -400,
+    delay: 3,
     duration: 3
+
+})
+
+
+
+
+// =======================
+// Third Section Animation
+// =======================
+gsap.from(".third-section", {
+    scrollTrigger:{
+    trigger: ".third-section",
+    
+    toggleActions: "restart resume resume restart"
+    } ,
+    opacity: 0,
+    delay: 1,
+    ease: Expo.easeInOut,
+    duration: 1
+
+})
+
+gsap.from(".title-1", {
+    scrollTrigger:{
+    trigger: ".third-section",
+    
+    toggleActions: "restart resume resume restart"
+    } ,
+    y: -200,
+    delay: 2,
+    duration: 2
+
+})
+
+gsap.from(".apps", {
+    scrollTrigger:{
+    trigger: ".third-section",
+    
+    toggleActions: "restart resume resume restart"
+    } ,
+    y: 400,
+    delay: 2,
+    duration: 2
+
+})
+
+
+// =======================
+// Forth Section Animation
+// =======================
+gsap.from(".forth-section", {
+    scrollTrigger:{
+    trigger: ".forth-section",
+   
+    toggleActions: "restart resume resume restart "
+    } ,
+    opacity: 0,
+    delay: 1,
+    ease: Expo.easeInOut,
+    duration: 1
+
+})
+gsap.from(".anim-left", {
+    scrollTrigger:{
+    trigger: ".anim-left",
+    toggleActions: "restart resume resume restart"
+    } ,
+    x: -200,
+    delay: 2,
+    ease: Expo.easeInOut,
+    duration: 2,
+
+
+})
+gsap.from(".anim-right", {
+    scrollTrigger:{
+    trigger: ".anim-right",
+    toggleActions: "restart resume resume restart"
+    } ,
+    x: 200,
+    delay: 2,
+    ease: Expo.easeInOut,
+    duration: 2,
+    
 
 })
 
